5515f7648577259aff0e8fd7e817360b8c0b9e60
neel_goswami <neelgoswami.cs@gmail.com>
2020-10-24 19:01:28 -0500

    API routes
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\api\AuthController;
+use App\Http\Controllers\api\BookController;
+use App\Http\Controllers\api\ReadingListController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,6 +21,21 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 	return $request->user();
 });
 
+// BOOK ROUTES
+Route::get('/books', [BookController::class, 'index']);
+Route::get('/books/{book}', [BookController::class, 'show']);
+
 // AUTH ROUTES
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+
+// READING LIST
+Route::middleware('auth:api')->group(function () {
+	Route::get('/lists', [ReadingListController::class, 'index']);
+	Route::get('/lists/{readingList}', [ReadingListController::class, 'show']);
+	Route::post('/lists', [ReadingListController::class, 'store']);
+	Route::put('/lists/{readingList}', [ReadingListController::class, 'update']);
+	Route::delete('/lists/{readingList}', [ReadingListController::class, 'destroy']);
+
+	Route::get('/lists/{readingList}/items', [ReadingListController::class, 'listItems']);
+});
