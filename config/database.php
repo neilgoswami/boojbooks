c41c4e05101256ff07756394644059cd8ef01950
neel_goswami <neelgoswami.cs@gmail.com>
2020-10-27 01:26:20 -0500

    Dynamic rds and local db setup

--- config/database.php
+++ config/database.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Str;
 
+$DB_HOST = !empty($_SERVER['RDS_HOSTNAME']) ? $_SERVER['RDS_HOSTNAME'] : env('DB_HOST', '127.0.0.1');
+$DB_PORT = !empty($_SERVER['RDS_PORT']) ? $_SERVER['RDS_PORT'] : env('DB_PORT', '3306');
+$DB_NAME = !empty($_SERVER['RDS_DB_NAME']) ? $_SERVER['RDS_DB_NAME'] : env('DB_DATABASE', 'forge');
+$DB_USERNAME = !empty($_SERVER['RDS_USERNAME']) ? $_SERVER['RDS_USERNAME'] : env('DB_USERNAME', 'forge');
+$DB_PASSWORD = !empty($_SERVER['RDS_PASSWORD']) ? $_SERVER['RDS_PASSWORD'] : env('DB_PASSWORD', '');
+
 return [
 
     /*
@@ -46,11 +52,11 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DB_HOST,
+            'port' => $DB_PORT,
+            'database' => $DB_NAME,
+            'username' => $DB_USERNAME,
+            'password' => $DB_PASSWORD,
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
